f3253e577a5340066566dcd4ade48ec26f96d838
joelmss93 <joelmss93@gmail.com>
2021-06-22 21:52:16 -0300

    feat: tags routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
 
 export const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post('/users', createUserController.handle);
+router.post('/tags', createTagController.handle);
