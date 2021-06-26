a963e0b68f336d746802826183b50e1d79eb9d22
joelmss93 <joelmss93@gmail.com>
2021-06-25 22:25:58 -0300

    feat: list user sender & receiver service

---
+++ src/services/ListUserReceiveComplimentsService.ts
@@ -0,0 +1,19 @@
+import { getCustomRepository } from 'typeorm';
+import { Compliment } from '../entities/Compliments';
+import { ComplimentsRepositories } from '../repositories/ComplimentsRepositories';
+
+export class ListUserReceiveComplimentsService {
+  async execute(user_id: string): Promise<Compliment | void> {
+    const complimentsRepositories = getCustomRepository(
+      ComplimentsRepositories,
+    );
+
+    const compliments = await complimentsRepositories.findOne({
+      where: {
+        user_receiver: user_id,
+      },
+    });
+
+    return compliments;
+  }
+}
